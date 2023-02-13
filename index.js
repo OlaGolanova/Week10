fdf1c150b8ad154e63bb4d011b0aeb482243f412
Ola <musicola@mail.ru>
2023-02-13 15:20:49 +0300

    add slider and answers to questions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Калькулятор
+
 
  function sum() {
      let x = prompt('Введите первое число');
@@ -20,4 +22,37 @@ function multiplication() {
     let x = prompt('Введите первое число');
     let y = prompt('Введите второе число');
     alert (x * y);
+}
+
+// Практическое задание
+
+function showMassage() {
+    console.log('Я учу JavaScript!');
+}
+showMassage();  //Я учу JavaScript!
+
+//
+
+
+function showImagePrev() {
+    let prev = document.getElementById('previous');
+    let next = document.getElementById('next');
+    let image = document.getElementById('slider-img');
+    previous.style.backgroundColor = 'pink';
+    previous.style.color = 'black';
+    next.style.backgroundColor = 'black';
+    next.style.color = 'antiquewhite';
+    image.src = 'slider/pineapple.jpg';
+}
+
+
+function showImageNext() {
+    let prev = document.getElementById('previous');
+    let next = document.getElementById('next');
+    let image = document.getElementById('slider-img');
+    next.style.backgroundColor = 'pink';
+    next.style.color = 'black';
+    previous.style.backgroundColor = 'black';
+    previous.style.color = 'antiquewhite';
+    image.src = 'slider/pear.jpg';
 }
